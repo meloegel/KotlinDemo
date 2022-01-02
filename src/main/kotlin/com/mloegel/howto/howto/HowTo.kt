7533974e408a6e594913bee2842820cb517619e3
meloegel <meloegel@gmail.com>
2022-01-01 19:46:03 -0500

    Working getAllHowtos and postHowto

--- src/main/kotlin/com/mloegel/howto/howto/HowTo.kt
+++ src/main/kotlin/com/mloegel/howto/howto/HowTo.kt
@@ -6,7 +6,7 @@ import org.springframework.data.relational.core.mapping.Table
 @Table("HOWTOS")
 data class HowTo(
     @Id
-    val id:String,
+    val id:String?,
     val name:String,
     val description: String,
     val category: String,
